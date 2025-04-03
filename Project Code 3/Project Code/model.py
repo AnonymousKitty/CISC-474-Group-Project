a9754bafeea5f33e7d1820416c7ed4363c5fc979
BCheng-KH <brandonkhcheng123@gmail.com>
2025-04-03 13:29:54 -0400

    test 6

---
+++ Project Code 3/Project Code/model.py
@@ -0,0 +1,108 @@
+from stable_baselines3 import DQN
+import gymnasium as gym
+import time
+import coverage_gridworld
+from stable_baselines3.common.env_util import make_vec_env
+from coverage_gridworld.custom import observation_space, observation, reward
+
+
+
+maps = [
+    [
+        [3, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ],
+    [
+        [3, 0, 0, 2, 0, 2, 0, 0, 0, 0],
+        [0, 2, 0, 0, 0, 2, 0, 0, 2, 0],
+        [0, 2, 0, 2, 2, 2, 2, 2, 2, 0],
+        [0, 2, 0, 0, 0, 2, 0, 0, 0, 0],
+        [0, 2, 0, 2, 0, 2, 0, 0, 2, 0],
+        [0, 2, 0, 0, 0, 0, 0, 2, 0, 0],
+        [0, 2, 2, 2, 0, 0, 0, 2, 0, 0],
+        [0, 0, 0, 0, 0, 2, 0, 0, 2, 0],
+        [0, 2, 0, 2, 0, 2, 2, 0, 0, 0],
+        [0, 0, 0, 0, 0, 2, 0, 0, 0, 0]
+    ],
+    [
+        [3, 2, 0, 0, 0, 0, 2, 0, 0, 0],
+        [0, 2, 0, 2, 2, 0, 2, 0, 2, 2],
+        [0, 2, 0, 2, 0, 0, 2, 0, 0, 0],
+        [0, 2, 0, 2, 0, 2, 2, 2, 2, 0],
+        [0, 2, 0, 2, 0, 0, 2, 0, 0, 0],
+        [0, 2, 0, 2, 2, 0, 2, 0, 2, 2],
+        [0, 2, 0, 2, 0, 0, 2, 0, 0, 0],
+        [0, 2, 0, 2, 0, 2, 2, 2, 2, 0],
+        [0, 2, 0, 2, 0, 4, 2, 4, 0, 0],
+        [0, 0, 0, 2, 0, 0, 0, 0, 0, 0]
+    ],
+    [
+        [3, 0, 2, 0, 0, 0, 0, 2, 0, 0],
+        [0, 0, 2, 0, 0, 0, 0, 0, 0, 4],
+        [0, 0, 2, 0, 0, 0, 0, 2, 0, 0],
+        [0, 0, 2, 0, 0, 0, 0, 2, 0, 0],
+        [0, 4, 2, 0, 0, 0, 0, 2, 0, 0],
+        [0, 0, 2, 0, 0, 0, 0, 2, 0, 0],
+        [0, 0, 2, 0, 0, 0, 0, 2, 0, 0],
+        [0, 0, 2, 0, 0, 0, 0, 2, 0, 0],
+        [0, 0, 0, 0, 4, 0, 4, 2, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 2, 0, 0]
+    ],
+    [
+        [3, 0, 0, 0, 0, 0, 0, 4, 0, 0],
+        [0, 2, 0, 2, 0, 0, 2, 0, 2, 0],
+        [0, 0, 0, 0, 4, 0, 0, 0, 0, 0],
+        [0, 2, 0, 2, 0, 0, 2, 0, 2, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 2, 0, 2, 0, 0, 2, 0, 2, 0],
+        [4, 0, 0, 0, 0, 0, 0, 0, 0, 4],
+        [0, 2, 0, 2, 0, 0, 2, 0, 2, 0],
+        [0, 0, 0, 0, 0, 4, 0, 0, 0, 0],
+        [0, 2, 0, 2, 0, 0, 2, 0, 2, 0]
+    ]
+]
+
+#env = gym.make("standard", predefined_map_list=maps)
+
+#env = gym.make("sneaky_enemies", render_mode="human", predefined_map_list=None, activate_game_status=True)
+# print("Env class chain:")
+# while hasattr(env, "env"):
+#     print("→", type(env))
+#     env = env.env
+# print("→", type(env)) 
+# print(type(env.unwrapped))
+# print((env.unwrapped.grid + 255).flatten())
+# print("one: ", gym.spaces.MultiDiscrete((env.unwrapped.grid + 255).flatten()))
+# print("two: ", observation_space(env.unwrapped))
+
+
+# Parallel environments for training
+do_training = True
+if do_training:
+    vec_env = make_vec_env("all_maps", n_envs=32)
+    model = DQN("MlpPolicy", vec_env, learning_rate=0.0005, learning_starts=10000, gamma=0.8, exploration_fraction=0.95, exploration_final_eps=0.1, verbose=1, tensorboard_log="./tensorboard/")
+    model.learn(total_timesteps=50000000, tb_log_name="dqn_run")
+    model.save("test6")
+
+# Single environment for testing
+env = gym.make("just_go", render_mode="human")
+model = DQN.load("test6")
+obs, _ = env.reset()
+terminated = False
+total_reward = 0
+while not terminated:
+    action, _states = model.predict(obs, deterministic=True)
+    obs, rewards, terminated, truncated, info = env.step(action)
+    total_reward += rewards
+    time.sleep(0.1)
+
+print(f"Total reward: {total_reward}")
+env.close()
